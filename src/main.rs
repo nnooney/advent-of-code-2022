c320ce02ac502d0e534d50abe19cca0dee5f9b38
Nicholas Nooney <nicholasnooney@gmail.com>
2022-12-04 16:11:04 -0800

    Add command line arguments for the different days

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,19 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
+use std::env;
+
+mod day1;
 
 fn main() {
-    day_one();
-}
+    let args: Vec<String> = env::args().collect();
 
-fn day_one() {
-    const DAY_ONE_FILE: &str = "./input/day1.txt";
-    let file = File::open(DAY_ONE_FILE).expect("Cannot open file");
-    let reader = BufReader::new(file);
+    if args.len() < 2 {
+        println!("Usage: {} [days...]", args[0]);
+        std::process::exit(1);
+    }
 
-    let mut vec: Vec<u64> = Vec::new();
-    let mut sum: u64 = 0;
-    for line in reader.lines() {
-        let line = line.expect("Cannot parse line");
-
-        if line == "" {
-            vec.push(sum);
-            sum = 0;
-        } else {
-            sum += line
-                .trim()
-                .parse::<u64>()
-                .expect("Cannot parse number from line");
+    for day in &args[1..] {
+        match &day[..] {
+            "1" => day1::day_one(),
+            day => println!("Day {} not implemented", day),
         }
     }
-
-    match vec.iter().max() {
-        Some(max) => println!("Max: {}", max),
-        None => println!("Empty vector"),
-    }
-
-    vec.sort_by(|a, b| b.cmp(a));
-    println!("Sum of top 3: {}", vec[0] + vec[1] + vec[2])
 }
